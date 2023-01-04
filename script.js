5b69add9b6393879d65f98ada29fd63f2b09da6e
manish prajapati <test@gmail.com>
2023-01-04 19:16:34 +0530

    my personal portfolio

---
+++ script.js
@@ -0,0 +1,22 @@
+let header = document.getElementById("header");
+let navbar = document.getElementById("navbar");
+let toggle_btn = document.getElementById("toggle_btn");
+window.addEventListener("scroll",function(e){
+    if(window.scrollY > 30){ header.classList.add("active") }else{ header.classList.remove("active") }
+});
+function togglenav(){
+    navbar.classList.toggle("active");
+}
+toggle_btn.addEventListener("click",togglenav);
+if(window.innerWidth < 900){
+    navbar.children[0].addEventListener("click",togglenav);
+}
+//toggle portfolio description
+let toggle_desc = document.querySelectorAll(".toggle_desc");
+toggle_desc.forEach(function(elem){
+    elem.addEventListener("click",function(){
+        elem.nextElementSibling.classList.toggle("active");
+        elem.children[0].classList.toggle("fa-plus");
+        elem.children[0].classList.toggle("fa-minus");
+    })
+});
